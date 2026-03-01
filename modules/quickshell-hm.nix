5ca824eb514311df048ea1a87a9309c8c18caea9
Kehribar <103407696+dpentx@users.noreply.github.com>
2026-03-01 16:30:48 +0300

    .

--- modules/quickshell-hm.nix
+++ modules/quickshell-hm.nix
@@ -1,28 +1,18 @@
 { config, pkgs, inputs, ... }:
 
 # modules/quickshell-hm.nix
-# Bu dosyayı projenin modules/ klasörüne koy.
-# QML dosyalarının ~/manix/qs/ altında olduğunu varsayar.
 
 {
-  # Quickshell binary'sini home ortamına ekle
   home.packages = [
     inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
-  # QML kaynaklarını ~/.config/quickshell/ altına semlink olarak koy
   xdg.configFile = {
-    "quickshell/shell.qml".source =
-      "${config.home.homeDirectory}/manix/qs/shell.qml";
-    "quickshell/TopBar.qml".source =
-      "${config.home.homeDirectory}/manix/qs/TopBar.qml";
-    "quickshell/LauncherOverlay.qml".source =
-      "${config.home.homeDirectory}/manix/qs/LauncherOverlay.qml";
-    "quickshell/modules/Clock.qml".source =
-      "${config.home.homeDirectory}/manix/qs/modules/Clock.qml";
-    "quickshell/modules/Workspaces.qml".source =
-      "${config.home.homeDirectory}/manix/qs/modules/Workspaces.qml";
-    "quickshell/modules/StatusIcons.qml".source =
-      "${config.home.homeDirectory}/manix/qs/modules/StatusIcons.qml";
+    "quickshell/shell.qml".source            = ../qs/shell.qml;
+    "quickshell/TopBar.qml".source           = ../qs/TopBar.qml;
+    "quickshell/LauncherOverlay.qml".source  = ../qs/LauncherOverlay.qml;
+    "quickshell/modules/Clock.qml".source        = ../qs/modules/Clock.qml;
+    "quickshell/modules/Workspaces.qml".source   = ../qs/modules/Workspaces.qml;
+    "quickshell/modules/StatusIcons.qml".source  = ../qs/modules/StatusIcons.qml;
   };
 }
